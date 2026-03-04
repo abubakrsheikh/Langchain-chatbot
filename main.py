e3c63725f3a0acc30e33e4414ee55a130ac444c0
Abubakr <abubakr.sheikh01@gmail.com>
2026-03-04 14:23:33 +0500

    :sparkles: Add LangChain Ollama setup

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from langchain-chatbot!")
+from langchain_ollama import ChatOllama
 
 
-if __name__ == "__main__":
-    main()
+llm = ChatOllama(
+    model="qwen2.5-coder:3b",
+    temperature=0.7
+)
+
+response = llm.invoke("What is RAG")
+print(response.content)
+
