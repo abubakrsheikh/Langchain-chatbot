969498d00c1cb98a6ec6258a36be07d37e376293
Abubakr <abubakr.sheikh01@gmail.com>
2026-03-04 15:55:09 +0500

    :sparkles: Add RAG implementation with streaming

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 
 llm = ChatOllama(
@@ -6,6 +9,23 @@ llm = ChatOllama(
     temperature=0.7
 )
 
-response = llm.invoke("What is RAG")
-print(response.content)
+messages = [
+    SystemMessage(content="You are a helpful assistant that translates English to French. Translate the user sentence."),
+    HumanMessage(content="I love programming.")
+    
+]
 
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful AI Assistant"),
+    ("human","{question}")
+])
+
+chain = prompt | llm | StrOutputParser()
+
+
+# response = chain.invoke({"question":"What is RAG?"})
+# print(response)
+
+for chunk in chain.stream({"question":"What is RAG?"}):
+    print(chunk, end="", flush=True)
+    
